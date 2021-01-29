c9501a75e7adf475b3e4b59f0ddab38ec82775dd
prettyirrelevant <ienioladewumi@gmail.com>
2021-01-29 14:44:14 +0100

    fix margin: Profile.js

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,9 +1,11 @@
 const Profile = () => {
   return (
-    <div className="flex flex-col justify-center items-center mt-40">
-      <h1 className="font-extrabold text-gray-300 text-7xl tracking-wide">
-        Coming Soon!
-      </h1>
+    <div className="mx-auto lg:px-56 px-8 container mt-40">
+      <div className="flex flex-col justify-center items-center">
+        <h1 className="font-extrabold text-gray-300 text-7xl tracking-wide">
+          Coming Soon!
+        </h1>
+      </div>
     </div>
   );
 };
